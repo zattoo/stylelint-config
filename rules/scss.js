ce1faff14062a0db3bb08024ac3c314ece565a75
Bogdan Plieshka <bogdan.plieshka@zattoo.com>
2018-02-08 11:19:10 +0100

    Fix at mixin and variables regexp bug

--- rules/scss.js
+++ rules/scss.js
@@ -7,11 +7,11 @@ module.exports = {
         'scss/at-function-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?$',
         'scss/at-import-no-partial-leading-underscore': true,
         'scss/at-import-partial-extension-blacklist': ['scss'],
-        'scss/at-mixin-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?(__([A-Z0-9]+([A-Z][a-z0-9])*)+)?$',
+        'scss/at-mixin-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?(__([a-zA-Z0-9])+)?$',
         'scss/dollar-variable-colon-space-after': 'always',
         'scss/dollar-variable-colon-space-before': 'never',
         'scss/dollar-variable-pattern': [
-            '^(([a-z0-9]+([A-Z][a-z0-9])*)*)+(_([a-z0-9]+([A-Z][a-z0-9])*)+){0,2}(__([a-zA-Z0-9]+([A-Z][a-z0-9])*)+)?$',
+            '^(([a-z0-9]+([A-Z][a-z0-9])*)*)+(_([a-z0-9]+([A-Z][a-z0-9])*)+){0,2}(__([a-zA-Z0-9])+)?$',
             {
                 message:
                     'Should be in this format [block_element]_property__modifier!',
