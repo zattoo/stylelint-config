cc29645f84951748f802d03efa2087ecce030e45
Denis Rul <que.etc@gmail.com>
2021-10-26 23:37:07 +0200

    Update

--- rules/scss.js
+++ rules/scss.js
@@ -1,30 +1,31 @@
 module.exports = {
-    customSyntax: 'postcss-scss',
-    plugins: [
-        'stylelint-scss',
-    ],
-    rules: {
-        'scss/at-extend-no-missing-placeholder': true,
-        'scss/at-function-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?$',
-        'scss/at-import-no-partial-leading-underscore': true,
-        'scss/at-import-partial-extension-blacklist': ['scss'],
-        'scss/at-mixin-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?(__([a-zA-Z0-9])+)?$',
-        'scss/dollar-variable-colon-space-after': 'always',
-        'scss/dollar-variable-colon-space-before': 'never',
-        'scss/dollar-variable-pattern': [
-            '^(([a-z0-9]+([A-Z][a-z0-9])*)*)+(_([a-z0-9]+([A-Z][a-z0-9])*)+){0,2}(__([a-zA-Z0-9])+)?$',
-            {
-                message:
-                    'Should be in this format [block_element]_property__modifier!',
-            },
-        ],
-        'scss/percent-placeholder-pattern': [
-            '^$',
-            {
-                message: 'Placeholders should not be used, use mixin instead!',
-            },
-        ],
-        'scss/operator-no-unspaced': true,
-        'scss/selector-no-redundant-nesting-selector': true,
+    overrides: {
+        files: ['**/*.scss'],
+        customSyntax: 'postcss-scss',
+        plugins: ['stylelint-scss'],
+        rules: {
+            'scss/at-extend-no-missing-placeholder': true,
+            'scss/at-function-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?$',
+            'scss/at-import-no-partial-leading-underscore': true,
+            'scss/at-import-partial-extension-blacklist': ['scss'],
+            'scss/at-mixin-pattern': '^[a-z]+([a-zA-Z0-9]+[a-z0-9]+)?(__([a-zA-Z0-9])+)?$',
+            'scss/dollar-variable-colon-space-after': 'always',
+            'scss/dollar-variable-colon-space-before': 'never',
+            'scss/dollar-variable-pattern': [
+                '^(([a-z0-9]+([A-Z][a-z0-9])*)*)+(_([a-z0-9]+([A-Z][a-z0-9])*)+){0,2}(__([a-zA-Z0-9])+)?$',
+                {
+                    message:
+                        'Should be in this format [block_element]_property__modifier!',
+                },
+            ],
+            'scss/percent-placeholder-pattern': [
+                '^$',
+                {
+                    message: 'Placeholders should not be used, use mixin instead!',
+                },
+            ],
+            'scss/operator-no-unspaced': true,
+            'scss/selector-no-redundant-nesting-selector': true,
+        },
     },
 };
